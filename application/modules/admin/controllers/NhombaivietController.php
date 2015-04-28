de1254788b544d50dc608583ea4aa90476d63a9a
Khien Pham <pckhien@gmail.com>
2015-04-28 07:22:27 +0700

    okey okey

--- application/modules/admin/controllers/NhombaivietController.php
+++ application/modules/admin/controllers/NhombaivietController.php
@@ -7,17 +7,25 @@ class Admin_NhombaivietController extends Zendvn_Controller_Action {
     }
 
     public function indexAction() {
+       //$nhomBaiVietTable = new Admin_Model_Nhombaiviet;
+        $arrayParent = $this->getArrayParent(null, 0, "--");
 
-        $nhomBaiVietTable = new Admin_Model_Nhombaiviet;
-        $arrayParent = $this->getArrayParent(null, 0, null);
-
-        $select = $nhomBaiVietTable->select()->order("idnhom_bai_viet DESC");
-        $listNhomBaiViet = $nhomBaiVietTable->fetchAll($select);
-
+//        $select = $nhomBaiVietTable->select()->order("idnhom_bai_viet DESC");
+//        $listNhomBaiViet = $nhomBaiVietTable->fetchAll($select);
+        
+//        echo "<pre>";
+//        print_r($listNhomBaiViet);
+//        echo "</pre>";
+//        die();
+//        echo "<pre>";
+//        print_r($this->getAllNhomBaiViet(null, 0));
+//        echo "</pre>";
+//        die();
+        $listNhomBaiViet = $this->getAllNhomBaiViet(null, 0);
         $this->view->listNhomBaiViet = $listNhomBaiViet;
         $this->view->arrayParent = $arrayParent;
         //$this->_helper->layout()->getView()->headTitle('View all students');
-        $this->view->headTitle('Index Nhom Bai Viet');
+        $this->view->headTitle('Danh sách nhóm bài viết');
     }
 
     public function addAction() {
@@ -44,13 +52,28 @@ class Admin_NhombaivietController extends Zendvn_Controller_Action {
     protected function getArrayParent($arrayParent, $parent, $style = "") {
         $nhomBaiVietTable = new Admin_Model_Nhombaiviet;
         $listNhomBaiViet = $nhomBaiVietTable->fetchAll(array("parent=?" => $parent));
-        $styles = $style . "--";
+        $styles = $style . " -- ";
         foreach ($listNhomBaiViet as $nhomBaiViet) {
-            $arrayParent[$nhomBaiViet['idnhom_bai_viet']] = $style . $nhomBaiViet['ten_nhom_bai_viet'];
+            $arrayParent[$nhomBaiViet['idnhom_bai_viet']] = $style." " . $nhomBaiViet['ten_nhom_bai_viet'];
             $arrayParent = $this->getArrayParent($arrayParent, $nhomBaiViet['idnhom_bai_viet'], $styles);
         }
 
         return $arrayParent;
+    }
+
+    protected function getAllNhomBaiViet($arrayParent, $parent) {
+        $nhomBaiVietTable = new Admin_Model_Nhombaiviet;
+        $listNhomBaiViet = $nhomBaiVietTable->fetchAll(array("parent=?" => $parent)); 
+        foreach ($listNhomBaiViet as $nhomBaiViet) {
+            $idNhomBaiViet = $nhomBaiViet->idnhom_bai_viet;
+            $tenNhomBaiViet = $nhomBaiViet->ten_nhom_bai_viet;
+            $parentNhomBaiViet = $nhomBaiViet->parent;
+            $arrayParent[] = array("idnhom_bai_viet"=>$idNhomBaiViet, "ten_nhom_bai_viet" =>$tenNhomBaiViet, "parent"=>$parentNhomBaiViet );
+            $arrayParent = $this->getAllNhomBaiViet($arrayParent, $nhomBaiViet['idnhom_bai_viet']);
+        }
+       
+        return $arrayParent;
+        
     }
 
 }
